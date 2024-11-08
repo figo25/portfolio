576a55c686bbe3755b3fdbb3f38577344cbabc35
Your Name <you@example.com>
2024-11-08 16:13:33 +0700

    first commit

---
+++ JS/particles.js
@@ -0,0 +1,50 @@
+/* ---- particles.js config ---- */
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 200,
+            "density": {
+                "enable": true,
+                "value_area": 700
+            }
+        },
+        "color": {
+            "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#fff"
+            }
+        },
+        "opacity": {
+            "value": 0.6,
+            "random": false,
+            "anim": {
+                "enable": false
+            }
+        },
+        "size": {
+            "value": 2,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 120,
+            "color": "#9b59b6",
+            "opacity": 0.4,
+            "width": 2
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            }
+        }
+    },
+    "retina_detect": true
+});
